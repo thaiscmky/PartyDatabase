cb8e62a2275da35fb68cc9eff1f209f3e73ec03b
Thais Cailet <thais.cailet@gmail.com>
2018-05-09 02:35:05 -0500

    Builds up ORM queries

--- config/orm.js
+++ config/orm.js
@@ -1,66 +1,68 @@
 var connection = require('./connection.js');
 
-var Orm = function(action, subjects, condition){
+function Orm(table, action, subjects, condition){
   this.sql = '';
   this.args = subjects !== null ? subjects : [];
-  this.table = null;
-  if(action !== null)
-      this.evaluateAction(action);
-  if(condition !== null)
-      this.evaluateCondition(condition);
-  this.setTable = function(tablename){
-      this.table = tablename;
-  };
-  this.evaluateAction = function(action){
-      switch(action.toLowerCase()){
-          case 'select':
-              this.sql.concat(this.sql, subjects !== null ? 'SELECT ?? FROM ?? ' : 'SELECT * FROM ?? ');
-              break;
-          case 'insert':
-              this.sql.concat(this.sql, 'INSERT INTO ?? SET ? ');
-              break;
-          case 'update':
-              this.sql.concat(this.sql, 'UPDATE ?? SET ? ');
-              break;
-          case 'delete':
-              this.sql.concat(this.sql, 'DELETE FROM ?? ');
-      }
-  };
-  this.evaluateCondition = function(condition) {
+  this.table = table;
+  this.action = action;
+  this.condition = condition;
+}
 
-      switch(condition){
-          case 'eq':
-              this.sql.concat(this.sql, 'WHERE ?? = ? ');
-              break;
-          case 'gt':
-              this.sql.concat(this.sql, 'WHERE ?? > ? ');
-              break;
-          case 'gte':
-              this.sql.concat(this.sql, 'WHERE ?? >= ? ');
-              break;
-          case 'lt':
-              this.sql.concat(this.sql, 'WHERE ?? < ? ');
-              break;
-          case 'lte':
-              this.sql.concat(this.sql, 'WHERE ?? <= ? ');
-              break;
-          case 'like':
-              this.sql.concat(this.sql, 'WHERE ?? LIKE ? ');
-              break;
-      }
-  };
-  this.runQuery = function(){
-      if(this.table !== null)   this.args.unshift(this.table);
-      if(this.sql.indexOf('SELECT') !== -1){
-          var temp = [this.args[1], this.args[0]];
-          this.args[0] = temp[0];
-          this.args[1] = temp[1];
-      }
-      connection.query(this.sql, this.args, function(error, results, fields){
-          if(error) throw error;
-          return results;
-      });
-  }
+Orm.prototype.constructor = Orm;
+
+Orm.prototype.evaluateAction = function(){
+    var sql = this.sql;
+    switch(this.action.toLowerCase()){
+        case 'select':
+            this.sql = sql.concat(sql, this.args !== null && this.args[0] !== '*' ? 'SELECT ?? FROM ?? ' : 'SELECT * FROM ?? ');
+            break;
+        case 'insert':
+            this.sql = sql.concat(sql, 'INSERT INTO ?? SET ? ');
+            break;
+        case 'update':
+            this.sql = sql.concat(sql, 'UPDATE ?? SET ? ');
+            break;
+        case 'delete':
+            this.sql = sql.concat(sql, 'DELETE FROM ?? ');
+    }
+
+};
+Orm.prototype.evaluateCondition = function() {
+    var sql = this.sql;
+    switch(this.condition){
+        case 'eq':
+            this.sql = sql.concat(sql, 'WHERE ?? = ? ');
+            break;
+        case 'gt':
+            this.sql = sql.concat(sql, 'WHERE ?? > ? ');
+            break;
+        case 'gte':
+            this.sql = sql.concat(sql, 'WHERE ?? >= ? ');
+            break;
+        case 'lt':
+            this.sql = sql.concat(sql, 'WHERE ?? < ? ');
+            break;
+        case 'lte':
+            this.sql = sql.concat(sql, 'WHERE ?? <= ? ');
+            break;
+        case 'like':
+            this.sql = sql.concat(sql, 'WHERE ?? LIKE ? ');
+            break;
+    }
+};
+Orm.prototype.runQuery = function(){
+    if(this['action'] !== null)
+        this.evaluateAction();
+    if(this['condition'] !== null)
+        this.evaluateCondition();
+
+    if(this.table !== null)   this.args.unshift(this.table);
+    if(this.sql.indexOf('SELECT') !== -1){
+        var temp = [this.args[1], this.args[0]];
+        this.args[0] = temp[0];
+        this.args[1] = temp[1];
+    }
+    return connection.query(this.sql, this.args);
 };
 
 module.exports = Orm;
--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-var orm = require('./config/orm.js');
+var Orm = require('./config/orm.js');
+
+var partyNames = new Orm('parties','select', ['party_name']);
+partyNames.runQuery()
+    .on('fields', function() {
+        console.log('Parties:');
+    })
+    .on('result', function(row){
+        console.log(row);
+});
+var clientNames = new Orm('clients', 'select', ['client_name']);
+clientNames.runQuery()
+    .on('fields', function() {
+        console.log('Clients:');
+    })
+    .on('result', function(row){
+    console.log(row);
+});
+var grownupParties = new Orm('parties', 'select', ['*', 'party_type', 'grown-up'], 'eq');
+grownupParties.runQuery()
+    .on('fields', function() {
+        console.log('Grownup Parties:');
+    })
+    .on('result', function(row){
+        console.log(row);
+    });
